3e0969f17da8d7bb135ceb1ef52cbacbf9e87c6e
Dawid0132 <92219386+Dawid0132@users.noreply.github.com>
2022-03-09 16:36:31 +0100

    Secound commit

---
+++ IdeaProjects/JTextArea/src/PoleTekstoweWW1.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class PoleTekstoweWW1 implements ActionListener {
+    JTextArea teskt;
+    
+    public static void main(String[] args){
+        PoleTekstoweWW1 gui = new PoleTekstoweWW1();
+        gui.tworzGUI();
+    }
+    public void tworzGUI(){
+        JFrame ramka = new JFrame();
+        JPanel panel = new JPanel();
+        ramka.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JButton przycisk = new JButton("Po prostu kliknij");
+        przycisk.addActionListener(this);
+        teskt = new JTextArea(10,20);
+        teskt.setLineWrap(true);
+
+        JScrollPane przewijanie = new JScrollPane(teskt);
+        przewijanie.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        przewijanie.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+
+        panel.add(przewijanie);
+
+        ramka.getContentPane().add(BorderLayout.CENTER, panel);
+        ramka.getContentPane().add(BorderLayout.SOUTH, przycisk);
+
+        ramka.setSize(350,300);
+        ramka.setVisible(true);
+    }
+    public void actionPerformed(ActionEvent zdarzenie){
+        teskt.append("przycisk kliknięty \n");
+    }
+}
